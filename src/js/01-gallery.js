d943fe4ef4923673e44e88aa621dc9052c573f02
SvistPetro <svist.petro@gmail.com>
2023-06-11 19:28:22 +0300

    Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,15 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
-console.log(galleryItems);
+const ulEl = document.querySelector('.gallery');
+const galleryElements = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<a href="${original}"><img class="gallery__image" src="${preview}" alt="" title="${description}"/></a>`
+);
+ulEl.insertAdjacentHTML('beforeend', galleryElements.join(''));
+
+const lightbox = new SimpleLightbox('.gallery a');
