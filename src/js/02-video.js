4f2513ea5fdeecd0284ae658fbffd39f762c4d45
SvistPetro <svist.petro@gmail.com>
2023-06-12 21:44:04 +0300

    Task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const TIME_KEY = 'videoplayer-current-time';
+const time = Number(localStorage.getItem(TIME_KEY));
+const throttle = require('lodash.throttle');
+
+const onPlay = function (data) {
+  localStorage.setItem(TIME_KEY, JSON.stringify(data.seconds));
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(time);
